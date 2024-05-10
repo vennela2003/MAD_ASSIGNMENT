601f1b70aadb3c58ea2a56ca557a4dd4cbe02100
vennela_sowjanya <94883183+vennela2003@users.noreply.github.com>
2024-05-11 01:02:12 +0530

    Add files via upload

---
+++ app/ApiService.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("v2/list")
+    Call<List<Image>> getImages(@Query("page") int page, @Query("limit") int limit);
+}
---
+++ app/Image.java
@@ -0,0 +1,21 @@
+public class Image {
+    private String id;
+    private String download_url;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDownloadUrl() {
+        return download_url;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.download_url = downloadUrl;
+    }
+}
